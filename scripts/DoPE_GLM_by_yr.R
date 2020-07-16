8dfa7f680d9b125fbbce70cafb2bee1e64b1b6e2
Robert Smith <rasmith3@sheffield.ac.uk>
2020-07-16 15:40:31 +0100

    attempting to add a random effects model - dataset too large though so abandonned.

--- scripts/DoPE_GLM_by_yr.R
+++ scripts/DoPE_GLM_by_yr.R
@@ -17,7 +17,7 @@ rm(list = ls())
 pacman::p_load(dplyr,reshape2,data.table,date,
                raster,geosphere,ggplot2,scales,
                RColorBrewer,miceadds,lubridate,
-               feather,stargazer, kableExtra,jtools)
+               feather,stargazer, kableExtra,jtools,lme4)
 
 # source all functions in R folder
 source.all(path = "R")
@@ -26,14 +26,14 @@ source.all(path = "R")
 # load data
 #=====#
 
-dt_parkrun <- readRDS("cleandata/lsoa_df_monthly.Rds")
-dt_parkrun$year = substr(x = dt_parkrun$month_year,start = 1,stop = 4) %>% as.numeric # create year variable
+dt_parkrun_month <- readRDS("cleandata/lsoa_df_monthly.Rds")
+dt_parkrun_month$year = substr(x = dt_parkrun_month$month_year,start = 1,stop = 4) %>% as.numeric # create year variable
 
 #====#
 # aggregate by year
 #====#
 
-dt_parkrun = dt_parkrun[,.(finishers = sum(finishers),
+dt_parkrun_yr = dt_parkrun_month[,.(finishers = sum(finishers),
               imd_score = mean(imd_score),
               mn_dstn = mean(access),
               pop_density = mean(pop_density),
@@ -55,10 +55,10 @@ f_model = function(x) {
   # create model based on data.
   model = glm(
     finishers ~ imd_score + perc_bme +  pop_density + mn_dstn + perc_non_working_age,
-    data = dt_parkrun,
+    data = dt_parkrun_yr,
     family = poisson(link = "log"),
     offset = log(total_pop),
-    subset =  which(dt_parkrun$year == x)
+    subset =  which(dt_parkrun_yr$year == x)
   )
   
   return(model)
@@ -99,6 +99,78 @@ stargazer(models[[1]],
           apply.coef = exp,
           apply.se   = exp,
           out = "outputs/results.html")
+
+
+#=============
+# ECONOMETRIC MODELS
+#=============
+
+dt_parkrun_month$obs_n = dt_parkrun_month$month_year %>%  as.factor() %>% as.numeric
+dt_parkrun_month$mn_dstn = dt_parkrun_month$access
+
+# 1. simple model adding month as a interaction term:
+panel_model = glm(
+    finishers ~ imd_score*obs_n + perc_bme +  pop_density + mn_dstn + perc_non_working_age,
+    data = dt_parkrun_month,
+    family = poisson(link = "log"),
+    offset = log(total_pop)
+    )
+
+stargazer(panel_model,
+          header = FALSE,
+          ci=FALSE, ci.level=0.95, #font.size= 9, 
+          title="Poisson Log-link GLM Results",
+          dep.var.labels = "Participation",
+          #covariate.labels = c("IMD Score",
+          #                     "Ethnic-Density",
+          #                     "Pop Density",
+          #                     "Distance(km)",
+          #                     "Non-working-age"),
+          type = "html",
+          apply.coef = exp,
+          apply.se   = exp,
+          out = "outputs/time_interaction_result.html")
+
+# 2. Can use a random effects model (GLMM) instead:
+# The Generalized Linear Mixed Model (GLMM) is an extension of the Generalized Linear Model (GLM) complicated by random effects
+# We use the glmer {lme4} function in R to reflect this.
+
+re_model2 = glmer(formula = finishers ~ 1+ year + perc_bme +  pop_density + mn_dstn + perc_non_working_age + (year | imd_score),
+      data = dt_parkrun_yr, 
+      family = poisson(link = "log"),
+      offset = log(total_pop))
+
+stargazer(panel_model2,
+          header = FALSE,
+          ci=FALSE, ci.level=0.95, #font.size= 9, 
+          title="Random Effects Poisson Log-link GLM Results",
+          #covariate.labels = c("IMD Score",
+          #                     "Ethnic-Density",
+          #                     "Pop Density",
+          #                     "Distance(km)",
+          #                     "Non-working-age"),
+          type = "html",
+          apply.coef = exp,
+          apply.se   = exp,
+          out = "outputs/time_interaction_result2.html")
+
+# 3. Another attempt (using a sample)
+dt_parkrun_yr
+
+re_model2 = glm(formula = finishers ~ year + imd_score + perc_bme +  pop_density + mn_dstn + perc_non_working_age +
+                                      year:imd_score + year:perc_bme + year:mn_dstn,
+                data = dt_parkrun_yr, 
+                family = poisson(link = "log"),
+                offset = log(total_pop))
+
+
+
+
+
+
+
+
+
 
 
 
